018416db1c7616269792a1cf7583f1606f116e70
Andrew Figaroa <andrewnumber1@gmail.com>
2019-07-05 15:12:34 -0400

    create tag v2.1

--- vpc.tf
+++ vpc.tf
@@ -7,16 +7,25 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "subnet_dmz" {
   cidr_block = "${var.subnet_dmz}"
   vpc_id = "${aws_vpc.main_vpc.id}"
-  tags {
-    Name = "dmz-subnet"
-  }
 }
 
-# subnrt internal
+# subnet internal
 resource "aws_subnet" "subnet_internal" {
   cidr_block = "${var.subnet_internal}"
   vpc_id = "${aws_vpc.main_vpc.id}"
-  tags {
-    Name = "internal-subnet"
+}
+
+# internet gateway
+resource "aws_nat_gateway" "nat_gatewat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${aws_subnet.subnet_dmz.id}"
+
+  tags = {
+    Name = "gw NAT"
   }
+}
+
+# EIP
+resource "aws_eip" "nat_eip" {
+  vpc = true
 }
