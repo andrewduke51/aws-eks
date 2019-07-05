5b32278fcd2bcce621548312be939ae2fd429168
Andrew Figaroa <andrewnumber1@gmail.com>
2019-07-05 14:49:25 -0400

    add tag v2.0

--- vpc.tf
+++ vpc.tf
@@ -7,11 +7,16 @@ resource "aws_vpc" "main_vpc" {
 resource "aws_subnet" "subnet_dmz" {
   cidr_block = "${var.subnet_dmz}"
   vpc_id = "${aws_vpc.main_vpc.id}"
+  tags {
+    Name = "dmz-subnet"
+  }
 }
 
 # subnrt internal
 resource "aws_subnet" "subnet_internal" {
   cidr_block = "${var.subnet_internal}"
   vpc_id = "${aws_vpc.main_vpc.id}"
+  tags {
+    Name = "internal-subnet"
+  }
 }
-
