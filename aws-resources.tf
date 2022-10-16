b391d2f9a86d6329790e9f2ca87d0ad0b432a8a7
AndrewF <andrew@sytrue.com>
2022-10-16 19:45:04 -0400

    Consolidate aws resources

---
+++ aws-resources.tf
@@ -0,0 +1,56 @@
+## VPC ##
+# Create a VPC using a remote module in github
+module "vpc" {
+  source = "git::https://github.com/andrewduke51/vpc-module.git"
+  subnet_dmz      = var.subnet_dmz
+  subnet_internal = var.subnet_internal
+  subnet_vpc      = var.subnet_vpc
+}
+
+## KMS ##
+resource "aws_kms_key" "general_bucket_key" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "key-alias" {
+  name          = "alias/general-bucket-key"
+  target_key_id = aws_kms_key.general_bucket_key.key_id
+}
+## S3 ##
+resource "aws_s3_bucket" "general" {
+  bucket = var.bucket_name
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        kms_master_key_id = aws_kms_key.general_bucket_key.arn
+        sse_algorithm     = "aws:kms"
+      }
+
+      bucket_key_enabled = true
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "block" {
+  bucket = aws_s3_bucket.general.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+## DYNAMODB ##
+resource "aws_dynamodb_table" "general_table" {
+  name           = "general-table"
+  read_capacity  = 20
+  write_capacity = 20
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
--- vpc.tf
+++
@@ -1,7 +0,0 @@
-# Create a VPC using a remote module in github
-module "vpc" {
-  source = "git::https://github.com/andrewduke51/vpc-module.git"
-  subnet_dmz      = var.subnet_dmz
-  subnet_internal = var.subnet_internal
-  subnet_vpc      = var.subnet_vpc
-}
