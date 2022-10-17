2f14d0f045c11f9f3a8d12be67db88fc77f57c7d
AndrewF <andrew@sytrue.com>
2022-10-17 11:30:22 -0400

    Use self-hosted runner

--- dyi-kubernetes.tf
+++ dyi-kubernetes.tf
@@ -1,7 +1,7 @@
 module "dyi_kube_cluster" {
   source                      = "./modules/ec2"
   tag_name                    = "dyi-kube"
-  control_plane               = 1
+  control_plane               = 0
   nodes                       = 0
   control_plane_instance_type = "t2.small"
   node_instance_type          = "t2.small"
